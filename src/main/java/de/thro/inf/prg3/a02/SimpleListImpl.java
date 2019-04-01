3b8c901390e959a5185941673bcd667546022a34
Daniel Kurzendorfer <daniel.kurzendorfer@gmail.com>
2019-04-01 10:49:46 +0200

    Element in Liste implementiert

--- src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
+++ src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
@@ -7,5 +7,27 @@ package de.thro.inf.prg3.a02;
 public class SimpleListImpl implements SimpleList {
 
 	// TODO: Implement the required methods.
+    class Element {
+        Element next;
+        Object obj;
+
+        public Element(Object input, Element elem) {
+            obj = input;
+            next = elem;
+        }
+
+    }
+
+    public void add(Object obj) {
+
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public SimpleList filter(SimpleFilter sl) {
+        return this;
+    }
 
 }
